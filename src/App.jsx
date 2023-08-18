a784bc6f3c21fc90aff4a4075cd85f0502f2bfd2
Koushik Ahmed <koushikxahmed@gmail.com>
2023-08-19 00:25:11 +0600

    question and answer added with test.

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,16 @@ import Header from "./Header";
 import Main from "./comp/Main";
 import Loader from "./comp/Loader";
 import Error from "./comp/Error";
+import StartScreen from "./comp/StartScreen";
+import Question from "./comp/Question";
 
 const initialState = {
   questions: [],
   // 'loading', "error", "ready", "active", "finished"
   status: "loading",
+  index: 0,
+  answer: null,
+  points: 0,
 };
 
 function reducer(state, action) {
@@ -15,21 +20,44 @@ function reducer(state, action) {
     case "dataReceived":
       return { ...state, questions: action.payload, status: "ready" };
     case "dataFailed":
-      return { ...state, state: "error" };
+      return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + 1
+            : state.points,
+      };
 
+    case "nextQuestion":
+      return { ...state, index: state.index + 1 };
+
+    // default case
     default:
       throw new Error("Action unknown");
   }
 }
 
 function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
   useEffect(() => {
     fetch("http://localhost:9000/questions")
       .then((res) => res.json())
-      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .then((data) => {
+        dispatch({ type: "dataReceived", payload: data });
+        console.log(data);
+      })
       .catch((err) => dispatch({ type: "dataFailed" }));
   }, []);
+  const numQuestions = questions.length;
   return (
     <>
       <div className="app">
@@ -37,6 +65,16 @@ function App() {
         <Main>
           {status === "loading" && <Loader />}
           {status === "error" && <Error />}
+          {status === "ready" && (
+            <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+          )}
+          {status === "active" && (
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+          )}
         </Main>
       </div>
     </>
